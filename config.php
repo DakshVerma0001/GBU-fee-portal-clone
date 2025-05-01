86f8b2f924f0201489df3bce86057fbb64c7b157
Daksh Verma <126685429+DakshVerma0001@users.noreply.github.com>
2025-05-01 15:37:46 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// config.php
+
+$host = "localhost";
+$user = "root"; // default user for XAMPP
+$password = ""; // default password for XAMPP
+$dbname = "fee_portal";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
